a649658702db65aad9671a781b908aa201e11d5b
Tamires Nogueira <131693476+nexusdei@users.noreply.github.com>
2023-08-07 10:29:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,16 @@
-const form = document.getElementById('form-tarefas');
+$(document).ready(function() {
+    $('#form-tarefas').submit(function(e) {
+        e.preventDefault();
 
-let linhas = '';
+        const inputNomeTarefa = $('#nome-tarefa');
+        const linha = `<li>${inputNomeTarefa.val()}</li>`;
 
-form.addEventListener('submit' ,function(e) {
+        $('.lista').append(linha);
 
-    e.preventDefault();
+        inputNomeTarefa.val('');
+    });
 
-    const inputNomeTarefa = document.getElementById('nome-tarefa');
-
-    let linha = '<ul>';
-
-    linha += `<li>${inputNomeTarefa.value}</li>`;
-
-    linha += '</ul>';
-
-    
-
-    linhas += linha;
-
-    const corpoTabela = document.querySelector('.lista');
-
-    corpoTabela.innerHTML = linhas
-
-    inputNomeTarefa.value = '';
-
-    
-
-    })
-
-$(document).ready(function() { 
-
-    $('.cadastra').on('click', function() {
-
-        const nomeTarefa = $('#nomeTarefa').val()
-
-        const li = document.createElement('li')
-
-            li.innerHTML = `${nomeTarefa}`
-
-            $('ul').on('click', 'li', function() {
-                $(this).addClass('feito');
-            });
-
-    })
-
-})
+    $(document).on('click', 'li', function() {
+        $(this).toggleClass('feito');
+    });
+});
